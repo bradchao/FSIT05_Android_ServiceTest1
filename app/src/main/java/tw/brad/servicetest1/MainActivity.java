08efa788dfeb5de3efbc77a20554cbb8967e2699
Brad <brad@brad.tw>
2018-10-21 11:45:13 +0800

    v1

---
+++ app/src/main/java/tw/brad/servicetest1/MainActivity.java
@@ -0,0 +1,76 @@
+package tw.brad.servicetest1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private MyReceiver myReceiver;
+    private IntentFilter filter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myReceiver = new MyReceiver();
+        filter = new IntentFilter();
+        filter.addAction("brad");
+
+        seekBar = findViewById(R.id.seekbar);
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(myReceiver, filter);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(myReceiver);
+    }
+
+    public void test1(View view) {
+        Intent intent = new Intent(this, MyService.class);
+        intent.putExtra("start", true);
+        startService(intent);
+    }
+    public void test2(View view) {
+        Intent intent = new Intent(this, MyService.class);
+        intent.putExtra("start", false);
+        startService(intent);
+    }
+    public void test3(View view) {
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
+    }
+
+    private class MyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int len = intent.getIntExtra("len", -1);
+            int now = intent.getIntExtra("now", -1);
+            if (len != -1){
+                seekBar.setMax(len);
+            }else if (now != -1){
+                seekBar.setProgress(now);
+            }
+
+
+
+        }
+    }
+
+}
---
+++ app/src/main/java/tw/brad/servicetest1/MyService.java
@@ -0,0 +1,79 @@
+package tw.brad.servicetest1;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MyService extends Service {
+    private MediaPlayer mediaPlayer;
+    private Timer timer;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (mediaPlayer != null) return;
+
+        Log.v("brad", "init");
+        mediaPlayer = MediaPlayer.create(this, R.raw.brad);
+        int len = mediaPlayer.getDuration();
+        Intent intent = new Intent("brad");
+        intent.putExtra("len", len);
+        sendBroadcast(intent);
+
+        timer = new Timer();
+        timer.schedule(new MyTask(), 0, 100);
+    }
+
+    private class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+                Intent intent = new Intent("brad");
+                intent.putExtra("now", mediaPlayer.getCurrentPosition());
+                sendBroadcast(intent);
+            }
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        boolean isStart = intent.getBooleanExtra("start", false);
+        if (isStart){
+            if (!mediaPlayer.isPlaying()) {
+                Log.v("brad", "pause => start");
+                mediaPlayer.start();
+            }
+        }else{
+            Log.v("brad", "pause");
+            if (mediaPlayer != null && mediaPlayer.isPlaying()) mediaPlayer.pause();
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v("brad", "die");
+        if (mediaPlayer != null){
+            mediaPlayer.stop();
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+        if (timer != null){
+            timer.cancel();
+            timer.purge();
+            timer = null;
+        }
+    }
+}
